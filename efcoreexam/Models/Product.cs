2b524191207b9c594561af6ec50001c668ada8b4
NewaR <sudipshrestha960@gmail.com>
2022-09-12 00:42:40 +0545

    efcoreexam

---
+++ efcoreexam/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace efcoreexam.Models
+{
+    public class Product
+    {
+        [Required]
+        [Column("Id")]
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
